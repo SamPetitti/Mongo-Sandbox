c76c4802cadcb90f5fcf26ed82176e546d2c430b
Sampjam <sampjam@gmail.com>
2021-12-17 13:58:50 -0500

    added find functions

--- demo.js
+++ demo.js
@@ -1,7 +1,8 @@
 const { MongoClient } = require('mongodb');
-
+const db = "sample_airbnb";
+const collection = "listingsAndReviews";
 async function main() {
-    const uri = "mongodb+srv://demo:*********@sp-training.uoozx.mongodb.net/?retryWrites=true&w=majority";
+    const uri = "mongodb+srv://demo:xx@sp-training.uoozx.mongodb.net/?retryWrites=true&w=majority";
 
     const client = new MongoClient(uri);
 
@@ -28,7 +29,8 @@ async function main() {
         //         bathrooms:4543
         //     }
         // ]);
-
+        //await findListingByName(client, "Cleveland Loft");
+        await findListingsWithMinimumBedroomsBathroomsAndMostRecentReviews(client, { minimumNumberOfBedrooms: 3, minimumNumberOfBedrooms: 2, maximumNumberOfResults: 5 });
 
 
     } catch (e) {
@@ -39,10 +41,12 @@ async function main() {
 
 }
 
-async function createMultipleListings(client, newListings){
+main().catch(console.error);
+
+async function createMultipleListings(client, newListings) {
     const result = await client.db("sample_airbnb")
-    .collection("listingsAndReviews")
-    .insertMany(newListings);
+        .collection("listingsAndReviews")
+        .insertMany(newListings);
     console.log("New listing with the following id: " + result.insertedId);
     console.log(`${result.insertedCount} new Listings with the following Id(s): `);
     console.log(result.insertedIds);
@@ -53,10 +57,35 @@ async function createListing(client, newListing) {
     const result = await client.db("sample_airbnb").collection("listingsAndReviews")
         .insertOne(newListing);
 
-   
 }
 
-main().catch(console.error);
+async function findListingByName(client, nameOfListing) {
+    const result = await client.db("sample_airbnb").collection("listingsAndReviews")
+        .findOne({ name: nameOfListing });
+
+    if (result) {
+        console.log(`Found a listing in the collection with the name '${nameOfListing}'`);
+        console.log(result);
+    } else {
+        console.log(`Could not find any results`);
+    }
+}
+
+async function findListingsWithMinimumBedroomsBathroomsAndMostRecentReviews(
+    client, {
+        minimumNumberOfBedrooms = 0,
+        minimumNumberOfBathrooms = 0,
+        maximumNumberOfResults = Number.MAX_SAFE_INTEGER
+    } = {}) {
+    const cursor = await client.db(db).collection(collection).find({
+        bedrooms: { $gte: minimumNumberOfBedrooms },
+        bathrooms: { $gte: minimumNumberOfBathrooms }
+    }).sort({ last_review: -1 })
+        .limit(maximumNumberOfResults);
+
+    const result = await cursor.toArray();
+    console.log(result);
+}
 
 async function listDatabases(client) {
     const dbList = await client.db().admin().listDatabases();
